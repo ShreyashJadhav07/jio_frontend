b153cabf4dd8b543db4a9f41b42db2f7f536e662
Shreyash Jadhav <shreyashjadhav913@gmail.com>
2025-07-13 16:28:42 +0530

    Add static export to fix build errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',  // Add this line
     images:{
         remotePatterns: [
             {
                 hostname: "image.tmdb.org"
-
             },
             {
                 hostname: "localhost",
             }
         ]
-
-
     }
 };
 
