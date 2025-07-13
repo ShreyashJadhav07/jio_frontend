d646502c013367a4b130e2b295a139e46cc1810d
Shreyash Jadhav <shreyashjadhav913@gmail.com>
2025-07-13 14:23:19 +0530

    Fix case sensitivity issue for Vercel build

--- src/app/tv/watch/page.jsx
+++ src/app/tv/watch/page.jsx
@@ -1,5 +1,4 @@
-
-import WishListButton from "@/components/atom/WishListButton";
+import WishlistButton from "@/components/atom/WishListButton";
 import { buttonVariants } from "@/components/ui/button";
 import { api, ENDPOINT } from "@/lib/api";
 import { FilmIcon } from "lucide-react";
@@ -20,14 +19,13 @@ const page = async ({ searchParams: { id,poster_path } }) => {
                     />
                     <div className="flex flex-wrap gap-4 px-4 lg:px-10 py-8 items-center">
                         <h1 className="text-2xl font-bold">{details.name}</h1>
-                        <WishListButton
-                         wishlist={{
+                      <WishlistButton
+                            wishlist={{
                                 id: details.id,
                                 name: details.name,
                                 media_type: details.media_type || "tv",
                                 poster_path:poster_path
                             }}
-                          
                         />
                         
                        
--- src/components/atom/WIshListButton.jsx
+++ src/components/atom/WIshListButton.jsx
@@ -6,7 +6,7 @@ import { Button } from "../ui/button";
 import { api, ENDPOINT } from "@/lib/api";
 import { toast } from "sonner";
 
-const WishListButton = ({ wishlist }) => {
+const WishlistButton = ({ wishlist }) => {
     const user = useSelector((state) => state.user);
     const [loading, setLoading] = useState(false);
     
@@ -49,4 +49,4 @@ const WishListButton = ({ wishlist }) => {
     );
 };
 
-export default WishListButton;
+export default WishlistButton;
